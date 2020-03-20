f32ffb0d87a989f89f5e3e397ee31937a2824cd6
Shawn DelPercio <sdelperc@gmail.com>
2020-03-20 12:44:57 -0400

    added db helpers and routing for resources

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const projectsRouter = require('./projects/projects-router');
+const resourcesRouter = require('./resources/resources-router');
 
 const server = express();
 
@@ -8,6 +9,7 @@ server.use(express.json());
 server.use(morgan('dev'));
 
 server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
 
 server.get('/', (req, res) => {
 	res.status(200).send('all good');
